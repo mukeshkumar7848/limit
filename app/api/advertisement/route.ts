224f2fb339fcbb8ca741d96f93d15d5440c51978
Mukesh Choudhary <namelessgaming085@gmail.com>
2026-02-06 01:13:05 +0530

    message

--- app/api/advertisement/route.ts
+++ app/api/advertisement/route.ts
@@ -3,11 +3,13 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const advertisementImageUrl = process.env.ADVERTISEMENT_IMAGE_URL || "";
   const redirectUrl = process.env.REDIRECT_URL || "";
+  const viewPop = process.env.VIEW_POP === "true";
 
   return NextResponse.json(
     {
       advertisementImageUrl,
       redirectUrl,
+      viewPop,
     },
     {
       headers: {
