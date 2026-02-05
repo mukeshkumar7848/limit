dbe127ad5b4f4e604a50cfcf0e06cbcc0975b2d3
Haseeb Ahmed Raza Khan <web.dev.haseeb@gmail.com>
2026-02-05 21:01:39 +0500

    create new api for advertizment

---
+++ app/api/advertisement/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+
+  return NextResponse.json(
+    {
+      advertisementImageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/960px-Image_created_with_a_mobile_phone.png",
+    },
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    }
+  );
+}
+
+export async function OPTIONS() {
+  return NextResponse.json(
+    {},
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    }
+  );
+}
